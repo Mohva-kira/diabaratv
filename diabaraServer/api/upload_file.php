e27ba7f8ca787743548050780269773e4ab309bb
Mohva-kira <mtandjo@gmail.com>
2022-05-11 11:39:19 +0000

    adding upload file and get artist file on the server

--- diabaraServer/api/upload_file.php
+++ diabaraServer/api/upload_file.php
@@ -9,29 +9,34 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 
 $data = json_decode(file_get_contents("php://input"));
 $target_dir = "../uploads/";
+$target_file= "";
+$target_image = "";
+if(isset($_FILES["File"]["name"] )){
 $target_file = $target_dir . basename($_FILES["File"]["name"]);
-$target_image = $target_dir . basename($_FILES["Image"]["name"]);
+}
+
+if(isset($_FILES["Image"]["name"])){
+    $target_image = $target_dir . basename($_FILES["Image"]["name"]);
+}
+
+
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
-if(isset($_FILES["File"])) {
+if(isset($_FILES["File"]) || isset($_FILES["Image"])) {
 
     // Check if $uploadOk is set to 0 by an error
-    if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-    } else {
-        if (move_uploaded_file($_FILES["File"]["tmp_name"], $target_file)) {
+        if (isset($_FILES["File"])) {
+            move_uploaded_file($_FILES["File"]["tmp_name"], $target_file);
             echo "The file ". htmlspecialchars( basename( $_FILES["File"]["name"])). " has been uploaded.";
-        } else {
-            echo "Sorry, there was an error uploading your file.";
         }
 
-        if (move_uploaded_file($_FILES["Image"]["tmp_name"], $target_image)) {
+        if (isset($_FILES["Image"])) {
+            move_uploaded_file($_FILES["Image"]["tmp_name"], $target_image);
             echo "The file ". htmlspecialchars( basename( $_FILES["Image"]["name"])). " has been uploaded.";
-        } else {
-            echo "Sorry, there was an error uploading your file.";
         }
-    }
+
+}else {
+    echo "Sorry, there was an error uploading your file.";
 }
 ?>
