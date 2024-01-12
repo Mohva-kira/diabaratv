03dc070868348b1052282fbc65d86027102a2a41
Mohamed Tandjigora <mtandjo@gmail.com>
2024-01-12 11:21:15 +0000

    debug

--- app/src/components/Header/Header.jsx
+++ app/src/components/Header/Header.jsx
@@ -221,7 +221,7 @@ const Header = () => {
 
               >
                 <div className=" text-white text-xs p-1"> 
-                  <p>{user.auth ? user.auth?.user?.username : user.user.username}</p>
+                  <p>{user?.auth ? user?.auth?.user?.username : user.user.username}</p>
                   
                 </div>
                 <div className="w-10 mt-1 "> 
