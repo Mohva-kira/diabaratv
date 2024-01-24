d9a5935e92ad09e42c953c4544437c7eb1462480
Mohamed Tandjigora <mtandjo@gmail.com>
2024-01-24 12:21:29 +0000

    chunk update for vite.config

--- app/vite.config.js
+++ app/vite.config.js
@@ -15,7 +15,18 @@ export default ({ mode }) => {
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
     port: 3000, // you can replace this port with any port
-  }
+  },
+  build: {
+    rollupOptions: {
+        output:{
+            manualChunks(id) {
+                if (id.includes('node_modules')) {
+                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                }
+            }
+        }
+    }
+}
 })
 
 };
