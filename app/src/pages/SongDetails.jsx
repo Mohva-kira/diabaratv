1640814048fb430b8083bc99dd57977ee71107fc
Mohamed Tandjigora <mtandjo@gmail.com>
2024-06-20 14:55:12 +0000

    correcting update...

--- app/src/pages/SongDetails.jsx
+++ app/src/pages/SongDetails.jsx
@@ -14,26 +14,26 @@ const SongDetails = () => {
     const { activeSong, isPlaying } = useSelector(state => state.player)
     const { data: songData, isFetching: isFetchingSongDetails, isSuccess: isSuccess, isError: error } = useGetSongDetailsQuery(songid)
     // const { data: relatedSong, isFetching: isFetchingRelatedSong, error} = useGetSongRelatedQuery(isSuccess? songData?.data?.id : "")
-    const  songs = isSuccess && songData.data.length > 0 ? songData : localStorage.getItem('songs') && JSON.parse(localStorage.getItem('songs'))
-    const { data: artistData, isFetching: isFetchingArtistDetails, isError: errorArtiste } = useGetArtistDetailsQuery(songData?.data?.attributes?.artist?.data.id )
-    
+    const songs = isSuccess && songData.data.length > 0 ? songData : localStorage.getItem('songs') && JSON.parse(localStorage.getItem('songs'))
+    const { data: artistData, isFetching: isFetchingArtistDetails, isError: errorArtiste } = useGetArtistDetailsQuery(songData?.data?.attributes?.artist?.data.id)
 
-   const relatedSong = songs?.data?.filter((song) => song.attributes.artist.data.id === Number(songData?.data?.attributes.artist?.data.id) )
+
+    const relatedSong = songs?.data?.filter((song) => song.attributes.artist.data.id === Number(songData?.data?.attributes.artist?.data.id))
 
     const handlePauseClick = () => {
         dispatch(playPause(false))
-      }
-    
-      const handlePlayClick = (song, i) => {
-          dispatch(setActiveSong({ song, i}))
-          dispatch(playPause(true))
-      }
-      
-      useEffect(() => {
-        
-      }, [songData])
+    }
 
-    if(isFetchingSongDetails ) return <Loader title="Searching song details" />
+    const handlePlayClick = (song, i) => {
+        dispatch(setActiveSong({ song, i }))
+        dispatch(playPause(true))
+    }
+
+    useEffect(() => {
+
+    }, [songData])
+
+    if (isFetchingSongDetails) return <Loader title="Searching song details" />
     if (error) return <Error />
 
     return (
@@ -41,35 +41,33 @@ const SongDetails = () => {
             {console.log('sond', songData)}
             {console.log('art', artistData)}
             <Helmet>
-        <meta name="description" content={"Description of your page"} />
-        <meta property="og:title" content={songData?.attributes?.name} />
-        <meta
-          property="og:description"
-          content={`Ecouter la musique de ${songData?.data.attributes?.artist?.data?.attributes.name} - ${songData?.data.attributes.name}`}
-        />
-        <meta
-          property="og:image"
-          content={`https://diabara.tv${songData?.data.attributes?.cover?.data[0].attributes.url}`}
-        />
-        <meta
-          property="og:url"
-          content={`https://diabara.tcv/songs/${songData?.data.id}`}
-        />
-        <meta property="og:type" content="website" />
-        <meta property="twitter:card" content="Diabara.tv" />
-      </Helmet>
+                <meta name="description" content={"Description of your page"} />
+                <meta property="og:title" content={songData?.attributes?.name} />
+                <meta
+                    property="og:description"
+                    content={`Ecouter la musique de ${songData?.data.attributes?.artist?.data?.attributes.name} - ${songData?.data.attributes.name}`}
+                />
+                <meta property="og:image" content={`https://api.diabara.tv${songData?.data.attributes?.cover?.data[0]?.attributes?.formats?.small?.url}`} />
+
+                <meta
+                    property="og:url"
+                    content={`https://diabara.tcv/songs/${songData?.data.id}`}
+                />
+                <meta property="og:type" content="website" />
+                <meta property="twitter:card" content="Diabara.tv" />
+            </Helmet>
             <DetailsHeader artiste_id="" songData={songData} artistData={artistData} />
 
 
             <div className="flex items-center justify-center w-full">
-                <SongCard 
-                detail={'w-full'}
-                key={songData?.data.id}
-                song={songData.data}
-                
-                isPlaying={isPlaying}
-                activeSong={activeSong}
-                data={relatedSong}
+                <SongCard
+                    detail={'w-full'}
+                    key={songData?.data.id}
+                    song={songData.data}
+
+                    isPlaying={isPlaying}
+                    activeSong={activeSong}
+                    data={relatedSong}
                 />
             </div>
 
@@ -88,13 +86,13 @@ const SongDetails = () => {
                 </div>
             </div>
 
-            <RelatedSongs 
-             data={relatedSong}
-             isPlaying={isPlaying}
-             activeSong={activeSong}
-            
-             handlePauseClick={handlePauseClick}
-             handlePlayClick={handlePlayClick}
+            <RelatedSongs
+                data={relatedSong}
+                isPlaying={isPlaying}
+                activeSong={activeSong}
+
+                handlePauseClick={handlePauseClick}
+                handlePlayClick={handlePlayClick}
             />
         </div>
     )
